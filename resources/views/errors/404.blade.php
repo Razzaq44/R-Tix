f27eca8a534167765bef32e2e00686f58d0a0363
Razzaq44 <razzaq.bowo@gmail.com>
2025-05-19 19:24:05 +0700

    update error page that have standalone layout

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,12 +1,23 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('R-Tix', 'Page Not Found')
+<head>
+    <title>404 Page Not Found</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=inter:400,500,600&display=swap" rel="stylesheet" />
+    @vite('resources/css/app.css')
+    @livewireStyles
+</head>
 
-@section('content')
+<body>
     <div class="text-center flex w-full h-full justify-center items-center flex-col">
         <flux:heading size="xl" class="text-red-500">404</flux:heading>
         <flux:text class="text-xl mb-8">We can't seem to find the page you're looking for.</flux:text>
-        <flux:button href="/" wire:navigate class="hover:bg-transparent! underline" variant="ghost">Back To Dashboard
-        </flux:button>
     </div>
-@endsection
+    @fluxScripts
+    @livewireScripts
+</body>
+
+</html>
--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -1,12 +1,24 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('R-Tix', 'Error Found')
+<head>
+    <title>Error</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=inter:400,500,600&display=swap" rel="stylesheet" />
+    @vite('resources/css/app.css')
+    @livewireStyles
+</head>
 
-@section('content')
+<body>
     <div class="text-center flex w-full h-full justify-center items-center flex-col">
-        <flux:text class="text-xl mb-8">There was a problem processing your request. We've been notified and are working on
+        <flux:text class="text-xl mb-8">There was a problem processing your request. We've been notified and are working
+            on
             it. Please try again in a few minutes.</flux:text>
-        <flux:button href="/" wire:navigate class="hover:bg-transparent! underline" variant="ghost">Back To Dashboard
-        </flux:button>
     </div>
-@endsection
+    @fluxScripts
+    @livewireScripts
+</body>
+
+</html>
