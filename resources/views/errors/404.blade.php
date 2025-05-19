9ec8ce074d308f70293f5d6646cea8ac81b52fff
Razzaq44 <razzaq.bowo@gmail.com>
2025-05-19 19:26:47 +0700

    Add Style and layout into 404 Error

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,23 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <title>404 Page Not Found</title>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=inter:400,500,600&display=swap" rel="stylesheet" />
-    @vite('resources/css/app.css')
-    @livewireStyles
-</head>
+@section('R-Tix', 'Page Not Found')
 
-<body>
+@section('content')
     <div class="text-center flex w-full h-full justify-center items-center flex-col">
         <flux:heading size="xl" class="text-red-500">404</flux:heading>
         <flux:text class="text-xl mb-8">We can't seem to find the page you're looking for.</flux:text>
+        <flux:button href="/" wire:navigate class="hover:bg-transparent! underline" variant="ghost">Back To Dashboard
+        </flux:button>
     </div>
-    @fluxScripts
-    @livewireScripts
-</body>
-
-</html>
+@endsection
